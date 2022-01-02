b655e65aa0371ceb62091270a31dd6d073c8988d
Felipe Kesley <fkesley.dev@gmail.com>
2022-01-02 17:05:41 -0300

    adjust

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -4,7 +4,49 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Button, Table, Form } from 'react-bootstrap'
 import { Cart, Container, Heading } from 'src/components/All'
 import { ChangeCart } from '../../store/actions/cart.action'
-import * as Style from './style'
+
+import styled from 'styled-components'
+
+export const Content = styled.div`
+	width: 100%;
+	position: relative;
+
+	.icon {
+		fill: ${({ theme }) => theme.colors.dark};
+		width: 1.8rem;
+		cursor: pointer;
+	}
+
+	.table > tbody {
+		vertical-align: middle;
+	}
+
+	.table tr {
+		text-align: left;
+	}
+
+	.table .td-img {
+		min-width: 2rem;
+	}
+
+	.table .remove {
+		width: 2.5rem;
+	}
+
+	.table .center {
+		text-align: center;
+	}
+`
+
+export const Hr = styled.hr`
+	border-color: ${({ theme }) => theme.colors.lightGray};
+	margin: 2rem 0 4rem 0;
+`
+
+export const Flex = styled.div`
+	display: flex;
+	justify-content: flex-end;
+`
 
 const CartPage = () => {
 	//redux
@@ -71,10 +113,10 @@ const CartPage = () => {
 	}
 
 	return (
-		<Style.Content>
+		<Content>
 			<Container>
 				<Heading type="h2">Your Cart</Heading>
-				<Style.Hr />
+				<Hr />
 
 				{cart.quantity > 0 ? (
 					<Form>
@@ -115,17 +157,17 @@ const CartPage = () => {
 								))}
 							</tbody>
 						</Table>
-						<Style.Flex>
+						<Flex>
 							<Button variant="dark" onClick={handleCheckout}>
 								proceed to checkout
 							</Button>
-						</Style.Flex>
+						</Flex>
 					</Form>
 				) : (
 					<p>Cart is empty</p>
 				)}
 			</Container>
-		</Style.Content>
+		</Content>
 	)
 }
 
--- src/pages/checkout/index.jsx
+++ src/pages/checkout/index.jsx
@@ -1,19 +1,61 @@
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
-import { useDispatch, useSelector } from 'react-redux'
-import { Button, Table, Form } from 'react-bootstrap'
-import { Cart, Container, Heading } from 'src/components/All'
-import { ChangeCart } from '../../store/actions/cart.action'
-import * as Style from './style'
+// import { useEffect, useState } from 'react'
+// import { useRouter } from 'next/router'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { Button, Table, Form } from 'react-bootstrap'
+import { Container, Heading } from 'src/components/All'
+// import { ChangeCart } from '../../store/actions/cart.action'
+
+import styled from 'styled-components'
+
+export const Content = styled.div`
+	width: 100%;
+	position: relative;
+
+	.icon {
+		fill: ${({ theme }) => theme.colors.dark};
+		width: 1.8rem;
+		cursor: pointer;
+	}
+
+	.table > tbody {
+		vertical-align: middle;
+	}
+
+	.table tr {
+		text-align: left;
+	}
+
+	.table .td-img {
+		min-width: 2rem;
+	}
+
+	.table .remove {
+		width: 2.5rem;
+	}
+
+	.table .center {
+		text-align: center;
+	}
+`
+
+export const Hr = styled.hr`
+	border-color: ${({ theme }) => theme.colors.lightGray};
+	margin: 2rem 0 4rem 0;
+`
+
+export const Flex = styled.div`
+	display: flex;
+	justify-content: flex-end;
+`
 
 const CheckoutPage = () => {
 	return (
-		<Style.Content>
+		<Content>
 			<Container>
 				<Heading type="h2">Checkout</Heading>
-				<Style.Hr />
+				<Hr />
 			</Container>
-		</Style.Content>
+		</Content>
 	)
 }
 
